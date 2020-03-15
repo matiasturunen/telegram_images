d1b6f7620ac145978fe26a536d59c7c56419821d
Matias Turunen <mt95@windowslive.com>
2020-03-15 09:41:37 +0200

    Unique file names and load largest images

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const telegram = new Telegram(process.env.BOT_TOKEN, {
 })
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => {
+	ctx.reply('Welcome, friend');
+	console.log('command: /start')
+});
 bot.on('message', (ctx) => {
 	if (ctx.update.message) {
 		console.log(ctx.update.message);
@@ -18,15 +21,18 @@ bot.on('message', (ctx) => {
 		}
 		if (ctx.update.message.photo) {
 			console.log('PHOTO INFO:', ctx.update.message.photo);
-			let p = ctx.update.message.photo[0].file_id;
+
+			let p = getLargestPhoto(ctx.update.message.photo, true);
 			telegram.getFile(p)
 			.then(file => {
 				console.log('FILE:', file);
 				console.log('Downloading file...');
-				// https://api.telegram.org/file/bot<token>/<file_path>
+
+				const end = file.file_path.split('.')[1]; // Just assuming that all photos have file end tag...
+
 				const imageoptions = {
 					url: `https://api.telegram.org/file/bot${ process.env.BOT_TOKEN }/${ file.file_path }`,
-					dest: path.join(__dirname, 'photos')
+					dest: path.join(__dirname, 'photos', file.file_unique_id + '.' + end) // Save with unique name
 				}
 
 				download.image(imageoptions)
@@ -37,9 +43,37 @@ bot.on('message', (ctx) => {
 			})
 			.catch(err => console.error(err))
 		}
+		if (ctx.update.message.caption) {
+			console.log('CAPTION:', ctx.update.message.caption);
+		}
 	}
 	console.log("");
 })
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('komento', (ctx) => {
+	console.log('GOT A COMMAND!!');
+	ctx.reply('I don\'t accept commands');
+})
 bot.launch()
 console.log('START!!')
+
+// Get largest photo from list. If dimensions=true, compares dimensions and total pixel area. If false, compares by file size
+function getLargestPhoto(photos, dimensions=true) {
+	let largest = 0;
+	let selected = photos[0].file_id;
+	photos.forEach(photo => {
+		if (dimensions) {
+			const area = photo.width * photo.height;
+			if (area > largest) {
+				largest = area;
+				selected = photo.file_id;
+			}
+		} else {
+			if (photo.size > largest) {
+				largest = photo.size;
+				selected = photo.file_id;
+			}
+		}
+	});
+
+	return selected;
+}
